e9c01d739876352c3811c1dde9082efdb61ad0b0
David Okaliwe <okaliwedavid@gmail.com>
2023-08-07 00:04:46 +0100

    Update

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const menuBtn =  document.querySelector('#menu-btn');
 const closeBtn = document.querySelector('#close-btn');
 
 menuBtn.addEventListener('click', () => {
-	sideMenu.style.display = 'block';
-})
+	sideMenu.classList.add('show');
+});
 
 closeBtn.addEventListener('click', () => {
-	sideMenu.style.display = 'none';
-})
+	sideMenu.classList.remove('show');
+});
