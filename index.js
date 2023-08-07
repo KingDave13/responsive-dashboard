f3a8de50a40c8c02c04756715095c0c546e4023b
David Okaliwe <okaliwedavid@gmail.com>
2023-08-07 21:25:47 +0100

    Update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const sideMenu = document.querySelector('aside');
 const menuBtn =  document.querySelector('#menu-btn');
 const closeBtn = document.querySelector('#close-btn');
-const themeToggler = document.querySelector(".themeToggler");
+const themeToggler = document.querySelector(".theme-toggler");
 
 menuBtn.addEventListener('click', () => {
 	sideMenu.classList.add('show');
@@ -11,3 +11,6 @@ closeBtn.addEventListener('click', () => {
 	sideMenu.classList.remove('show');
 });
 
+themeToggler.addEventListener('click', () => {
+	document.body.classList.toggle('dark-theme-variables');
+})
